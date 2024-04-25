2fe05a02c5b3ab9c84b87793bd69fbb1fe3c8b6b
rexrothctrlxid <rexrothiot@gmail.com>
2024-04-25 22:26:44 +0700

    percobaan pertama

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css';
+import Home from './pages/Home';
+import NotFound from './components/NotFound';
+import Modbus from './components/comp/Modbus/Modbus';
+import Sorting from './components/comp/Sorting/Sorting';
+
+
+const App = () => {
+  return (      
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+        <Route path='/modbus' element={<Modbus />} />
+        <Route path='/sorting' element={<Sorting />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
