c62d192a28b0c823d158701d6864afbb73c91d4e
rexrothctrlxid <rexrothiot@gmail.com>
2024-04-25 22:52:39 +0700

    new update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, HashRouter, Link } from 'react-router-dom'
 import './App.css';
 import Home from './pages/Home';
 import NotFound from './components/NotFound';
@@ -8,14 +8,25 @@ import Sorting from './components/comp/Sorting/Sorting';
 
 const App = () => {
   return (      
-    <Router>
-      <Routes>
-        <Route path='https://aardiiaansyaahhhtnjng.github.io/belajar/' element={<Home />} />
-        <Route path='*' element={<NotFound />} />
-        <Route path='/modbus' element={<Modbus />} />
-        <Route path='/sorting' element={<Sorting />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path='https://aardiiaansyaahhhtnjng.github.io/belajar/' element={<Home />} />
+    //     <Route path='*' element={<NotFound />} />
+    //     <Route path='/modbus' element={<Modbus />} />
+    //     <Route path='/sorting' element={<Sorting />} />
+    //   </Routes>
+    // </Router>
+    <HashRouter basename='/'>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/modbus">Modbus</Link></li>          
+        </ul>
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route path="/modbus" component={Modbus} />
+      </div>
+    </HashRouter>
   );
 }
 
