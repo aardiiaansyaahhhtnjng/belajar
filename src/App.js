b8c2495c78667db898a8a623cc61fe16d17c8ccb
rexrothctrlxid <rexrothiot@gmail.com>
2024-04-25 23:03:59 +0700

    newest update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, HashRouter, Link } from 'react-router-dom'
+import { BrowserRouter as 
+  // Router, 
+  // Routes, 
+  Route, HashRouter, 
+  // Link 
+} from 'react-router-dom'
 import './App.css';
 import Home from './pages/Home';
-import NotFound from './components/NotFound';
+// import NotFound from './components/NotFound';
 import Modbus from './components/comp/Modbus/Modbus';
-import Sorting from './components/comp/Sorting/Sorting';
+// import Sorting from './components/comp/Sorting/Sorting';
 
 
 const App = () => {
@@ -18,13 +23,13 @@ const App = () => {
     // </Router>
     <HashRouter basename='/'>
       <div>
-        <ul>
+        {/* <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/modbus">Modbus</Link></li>          
-        </ul>
+        </ul> */}
         <hr />
-        <Route exact path="/" component={Home} />
-        <Route path="/modbus" component={Modbus} />
+        <Route exact path="/" component={<Home />} />
+        <Route path="/modbus" component={<Modbus />} />
       </div>
     </HashRouter>
   );
